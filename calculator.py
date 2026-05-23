7b737dc282a3756a191a422bfb17763840e92d47
Kabir Singh <146543182+kabir-singh-coder@users.noreply.github.com>
2026-05-23 22:45:20 +0530

    c

--- calculator.py
+++ calculator.py
@@ -1,27 +1,51 @@
 print("SMART CALCULATOR")
 
 while True:
-    print("\n1.Add  2.Subtract  3.Multiply  4.Divide  5.Exit")
-    ch = input("Choice: ")
 
-    if ch == "5":
-        print("Bye")
-        break
+    print("\n========== MENU ==========")
 
-    if ch == "1" or ch == "2" or ch == "3" or ch == "4":
-        a = float(input("Enter first number: "))
-        b = float(input("Enter second number: "))
+        print("1. Addition")
+            print("2. Subtraction")
+                print("3. Multiplication")
+                    print("4. Division")
+                        print("5. Exit")
 
-        if ch == "1":
-            print("Result =", a + b)
-        elif ch == "2":
-            print("Result =", a - b)
-        elif ch == "3":
-            print("Result =", a * b)
-        elif ch == "4":
-            if b == 0:
-                print("Cannot divide by zero")
-            else:
-                print("Result =", a / b)
-    else:
-        print("Invalid choice")
+                            ch = input("Enter Choice: ")
+
+                                if ch == "5":
+
+                                        print("Calculator Closed")
+
+                                                break
+
+                                                    elif ch == "1" or ch == "2" or ch == "3" or ch == "4":
+
+                                                            a = float(input("Enter First Number: "))
+
+                                                                    b = float(input("Enter Second Number: "))
+
+                                                                            if ch == "1":
+
+                                                                                        print("Addition =", a + b)
+
+                                                                                                elif ch == "2":
+
+                                                                                                            print("Subtraction =", a - b)
+
+                                                                                                                    elif ch == "3":
+
+                                                                                                                                print("Multiplication =", a * b)
+
+                                                                                                                                        elif ch == "4":
+
+                                                                                                                                                    if b == 0:
+
+                                                                                                                                                                    print("Division By Zero Not Possible")
+
+                                                                                                                                                                                else:
+
+                                                                                                                                                                                                print("Division =", a / b)
+
+                                                                                                                                                                                                    else:
+
+                                                                                                                                                                                                            print("Invalid Choice")
